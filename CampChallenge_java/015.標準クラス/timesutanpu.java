23f609adbc7c083ad0d1261d978cb1f175e84aa0
EXP-0017\guest1Day <test@gmail.com>
2018-04-06 19:10:17 +0900

    タイムスタンプ　課題

---
+++ CampChallenge_java/015.標準クラス/timesutanpu.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hyoujyun;
+
+import java.util.Date;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class timesutanpu {
+    
+    public static void main (String[] args){
+        
+        Date d = new Date(35*365*24*60*60*1000L+11*366*24*60*60*1000L-9*60*60*1000L);
+      
+        System.out.print(d);
+       
+        
+        
+ }
+    
+}
