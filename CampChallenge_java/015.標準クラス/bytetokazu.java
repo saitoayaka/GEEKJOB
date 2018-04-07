3149df214a9533b1d85e78f77a8e9e35b845df32
EXP-0017\guest1Day <test@gmail.com>
2018-04-07 11:31:44 +0900

    バイト数と文字数　課題

---
+++ CampChallenge_java/015.標準クラス/bytetokazu.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hyoujyun;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class bytetokazu {
+    public static void main(String[] args){
+      String name ="齋藤彩花";
+      
+      System.out.print("文字数は");
+      System.out.print(name.length());
+      System.out.print("バイト数は");
+      System.out.print(name.getBytes().length);
+    
+    }
+    
+}
